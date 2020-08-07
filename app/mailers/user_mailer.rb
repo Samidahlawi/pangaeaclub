8aa402073085bdd93ae5a6f779eaae24b0df0fdf
samidahlawi <o.t5@hotmail.com>
2020-08-07 19:45:01 +0300

    just added a normal mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ApplicationMailer
+  # default from: "info@pangaeaclub.com"
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.welcome_mailer.subject
+  #
+  # def welcome_mailer(user)
+  #   @greeting = "Hi"
+  #   @user = user 
+  #   mail to: user.email , subject: "مرحبا بك في PangaeaClub", from: "info@pangaeaclub.com"
+  # end
+end
